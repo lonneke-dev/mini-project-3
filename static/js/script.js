280fcd4ba2c209ce5f82b839c7484c3f2c467184
lonneke-dev <lonneke1908@outlook.com>
2021-04-03 10:32:50 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        fromat: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
